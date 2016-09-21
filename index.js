8a1032c95e7d0f88bf19d70f34d1e2d71d34c098
Dharmesh Khandelwal <dharmesh.khandelwal@mindtree.com>
2016-09-21 18:22:02 +0530

    Updated heading

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static('public'));
-
 var port = process.env.PORT || 4005;
 
 var server = app.listen(port, function(req, res) {
-    console.log("server running at port" + port);
+    console.log("server running at port " + port);
 })
